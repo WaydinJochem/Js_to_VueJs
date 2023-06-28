1060de70ec9de9ce8e3bf32341689a26d4127e81
WaydinJochem <130061226+WaydinJochem@users.noreply.github.com>
2023-06-28 13:38:46 +0000

    Changes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,17 +3,48 @@
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div> -->
-  <HomePageComp/>
+  <section>
+    <h1 id="Store">GuNs GaRloRe</h1>
+    <div id="intro">
+      <p>Your One Stop Store for Guns and More</p>
+      <br />
+      <p>Shop no further than Guns Garlore</p>
+    </div>
+    <div>
+      <h2 id="promo">Popular Products</h2>
+      <section id="added"></section>
+    </div>
+  </section>
 </template>
 
 <script>
 // @ is an alias to /src
-// import HelloWorld from "@/components/HelloWorld.vue";
-import HomePageComp from "@/components/HomeComp.vue"
+
 
 export default {
-  components: {
-   HomePageComp
-  }
+  
 }
 </script>
+<style>
+#first-view {
+  background-image: url ("src/assets/istockphoto-1205603080-612x612.jpg");
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+  padding: 2%;
+  color: aliceblue;
+}
+
+h1 {
+  text-align: center;
+  font-size: xxx-large;
+  text-shadow: 10px 10px #333;
+}
+
+#intro {
+  display: flex;
+  flex-direction: column;
+  align-items: end;
+  text-align: center;
+  margin: 5%;
+}
+</style>
